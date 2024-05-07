0887491913e3049ed26820c776a201a313d92195
Adill00 <coderguy915@gmail.com>
2024-05-08 00:08:55 +0400

    created city & country entities

--- src/screens/City.js
+++ src/screens/City.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { SafeAreaView,Text,StyleSheet } from 'react-native'
+import { SafeAreaView,Text,StyleSheet,ImageBackground,StatusBar } from 'react-native'
 
 
 const City = () => {
     return (
         <SafeAreaView style={styles.container}>
-            <Text>City</Text>
+            <ImageBackground source={require('../../assets/city-background.jpg')} style={styles.imageLayout}>
+                <Text style={styles.cityName}>London</Text>
+                <Text style={styles.countryName}>UK</Text>
+            </ImageBackground>
         </SafeAreaView>
     )
 }
@@ -14,7 +17,24 @@ const City = () => {
 const styles = StyleSheet.create({
     container:{
         flex:1,
-
+        marginTop:StatusBar.currentHeight || 0
+    },
+    imageLayout:{
+        flex:1
+    },
+    cityName:{
+        justifyContent:'center',
+        alignSelf:'center',
+        fontSize:40,
+        fontWeight:'bold',
+        color:'white'
+    },
+    countryName:{
+        justifyContent:'center',
+        alignSelf:'center',
+        fontSize:30,
+        fontWeight:'bold',
+        color:'white'
     }
 
 })
