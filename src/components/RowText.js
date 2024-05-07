0c7997ccb366d4e16c05b5740297d9973733e278
Adill00 <coderguy915@gmail.com>
2024-05-08 01:59:22 +0400

    refactoring CurrentWeather 1: style

---
+++ src/components/RowText.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+
+const RowText = (props) => {
+    const{messageOne,messageTwo,containerStyles,messsageOneStyles,messsageTwoStyles} = props 
+    return(
+        <View style={containerStyles}>
+          <Text style={messsageOneStyles}>{messageOne}</Text>
+          <Text style={messsageTwoStyles}>{messageTwo}</Text>
+        </View>
+    )
+}
+
+
+
+export default RowText
--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { View,Text,SafeAreaView,StyleSheet} from 'react-native'
 import {Feather} from '@expo/vector-icons'
+import RowText from "../components/RowText"
 
 
 const CurrentWeather = () => {
@@ -10,15 +11,24 @@ const CurrentWeather = () => {
       <Feather name="sun" size={100} color="black"/>
         <Text style={styles.temp}>6</Text>
         <Text style={styles.feels}>Feels like 5</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>High: 8 </Text>
-          <Text style={styles.highLow}>Low: 6 </Text>
-        </View>
+
+        <RowText 
+          messageOne={'High: 8'}
+          messageTwo={'Low: 6'}
+          containerStyles={styles.highLowWrapper}
+          messageOneStyles={styles.highLow}
+          messageTwoStyles={styles.highLow}
+        />
+        
       </View>
-      <View style={styles.bodyWrapper}>
-          <Text style={styles.description}>It is sunny</Text>
-          <Text style={styles.message}>It is t-shirt weather</Text>
-      </View>
+      <RowText 
+          messageOne={'its sunny'}
+          messageTwo={'its tshirt weather'}
+          containerStyles={styles.bodyWrapper}
+          messageOneStyles={styles.description}
+          messageTwoStyles={styles.message}
+      />
+
     </SafeAreaView>
   )
 }
