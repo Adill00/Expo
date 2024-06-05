c21031c5d31324fe79f9f23f345f5aa45116708a
Adill00 <coderguy915@gmail.com>
2024-06-05 16:39:59 +0400

    prep for navigation

--- src/components/RowText.js
+++ src/components/RowText.js
@@ -2,7 +2,12 @@ import React from 'react'
 import {View,Text} from 'react-native'
 
 const RowText = (props) => {
-    const{messageOne,messageTwo,containerStyles,messsageOneStyles,messsageTwoStyles} = props 
+    const{messageOne,
+      messageTwo,
+      containerStyles,
+      messsageOneStyles,
+      messsageTwoStyles} 
+    = props 
     return(
         <View style={containerStyles}>
           <Text style={messsageOneStyles}>{messageOne}</Text>
--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -5,28 +5,37 @@ import RowText from "../components/RowText"
 
 
 const CurrentWeather = () => {
+  const{container,
+    wrapper,
+    temp,
+    feels,
+    highLowWrapper,
+    highLow,
+    bodyWrapper,
+    description,
+    message} = styles
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style = {styles.container}>
+    <SafeAreaView style={wrapper}>
+      <View style = {container}>
       <Feather name="sun" size={100} color="black"/>
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
+        <Text style={temp}>6</Text>
+        <Text style={feels}>Feels like 5</Text>
 
         <RowText 
-          messageOne={'High: 8'}
+          messageOne={'High: 8 '}
           messageTwo={'Low: 6'}
-          containerStyles={styles.highLowWrapper}
-          messageOneStyles={styles.highLow}
-          messageTwoStyles={styles.highLow}
+          containerStyles={highLowWrapper}
+          messageOneStyles={highLow}
+          messageTwoStyles={highLow}
         />
         
       </View>
       <RowText 
           messageOne={'its sunny'}
           messageTwo={'its tshirt weather'}
-          containerStyles={styles.bodyWrapper}
-          messageOneStyles={styles.description}
-          messageTwoStyles={styles.message}
+          containerStyles={bodyWrapper}
+          messageOneStyles={description}
+          messageTwoStyles={message}
       />
 
     </SafeAreaView>
